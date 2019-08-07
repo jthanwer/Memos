b088a5323b84ff7cc58cacdf13833bb328ed9a33
jthanwer <joel.thanwerdas@gmail.com>
2019-08-07 09:05:02 +0200

    New features

--- nco/bash_ex/multiplier.sh
+++
@@ -1,6 +0,0 @@
-# multiplie le premier 85.nc par 3 et le deuxième 85.nc par -2 puis les ajoute.
-ncflint -w 3,-2 85.nc 85.nc tst.nc
-
-#http://nco.sourceforge.net/nco.html#xmp_ncflint
-#ncflint -w 0.8,0.0 extsw.nc extsw.nc tst.nc 
-#ncks -A -v time  extsw.nc tst.nc 
